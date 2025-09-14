3eaa283c262d9d70c1c77166800175c020e913f3
HattMayes <mattjhayes01@gmail.com>
2025-09-14 18:18:54 +0100

    Create Project Main.sql

---
+++ Project 1/Project Main.sql
@@ -0,0 +1,17 @@
+-- PROJECT 1 -
+
+-- Project Goals -
+/* 
+1. Analyse top-paying roles and skills from Luke Barousse's job data
+2. Be able to search the data to extract what skills I should target for which roles
+3. Showcase understanding of basic and more advanced SQL functions/operations
+/*
+
+-- Questions this project will answer -
+/* 
+- What are the top paying jobs for my chosen role?
+- What skills are required for these top-paying roles?
+- What are the top skills based on salary for my chosen role?
+- What are the most optimal skills to learn i.e. for a high demand and high paying job
+*/
+
